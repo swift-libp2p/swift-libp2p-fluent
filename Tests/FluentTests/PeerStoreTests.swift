6f2eb03e0eefc5c6f3b075aed9935dd2947c1613
Brandon <32753167+btoms20@users.noreply.github.com>
2026-02-15 11:14:35 -0800

    formatting

--- Tests/FluentTests/PeerStoreTests.swift
+++ Tests/FluentTests/PeerStoreTests.swift
@@ -18,17 +18,17 @@
 
 import Fluent
 import LibP2P
+import LibP2PCore
 import LibP2PCrypto
 import LibP2PTesting
 import Testing
 import XCTFluent
-import LibP2PCore
 
 @Suite("PeerStore Tests")
 struct PeerStoreTests {
-    
+
     let test = ArrayTestDatabase()
-    
+
     private func configure(_ app: Application) async throws {
         // Set our log level
         app.logger.logLevel = .info
@@ -37,20 +37,20 @@ struct PeerStoreTests {
         app.peerstore.use(.fluent)
         app.peerstore.prepareMigrations()
     }
-    
+
     @Test func peerStoreMigrationName() {
         #expect(PeerStoreEntry.migration.name == "Fluent.PeerStoreEntry.Create")
     }
-    
+
     @Test func testPeerStoreStoreAndFetchPeerID() async throws {
         try await withApp(configure: configure) { app in
             let peer = try PeerID(.Ed25519)
             test.append([TestOutput(PeerStoreEntry(id: UUID(), peerID: peer))])
-            
+
             do {
                 let peers1 = try await app.peers.all().get()
                 #expect(peers1.count == 1)
-                let recoveredPeer = try #require( peers1.first )
+                let recoveredPeer = try #require(peers1.first)
                 #expect(recoveredPeer.id == peer)
             } catch {
                 Issue.record(error)
